758a973eab04e26bd5aaccde349eacde9edf7119
Elmurod Vokhidov <elmurodvkh@gmail.com>
2023-09-01 14:56:39 +0500

    completed navbar component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Main from './components/Main';
 import Login from './components/Login';
 import Register from './components/Register';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/login' element={<Login />} />
-        <Route path='register' element={<Register />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </div>
   );
